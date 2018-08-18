e315b88be3240a345ecdedc698f88c55f039f3aa
Jay Honnold <jayhonnold@gmail.com>
2018-08-17 19:52:37 -0500

    Custom loader, assets from CDN

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,19 +1,17 @@
-// const $ = window.$;
 // import './lib/foundation';
 // $(document).foundation();
+const $ = window.$;
 
 $(document).ready(() => {
   $('main').fullpage({
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: [
-      'Home',
-      'About',
-      'Experience',
-      'Education',
-      'Skills',
-    ],
+    navigationTooltips: ['Home', 'About', 'Experience', 'Education', 'Skills'],
     sectionSelector: 'section',
     scrollOverflow: true,
   });
+
+  $('#styles-mount').html($('#deferred-styles').text());
+  $('#deferred-styles').remove();
+  setTimeout(() => $('#loader').remove(), 1000);
 });
